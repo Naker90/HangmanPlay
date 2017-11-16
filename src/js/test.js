f664d4417439cefee07ca63b267bd6ec83dc43e3
naker90 <AntonioJesusSG96@gmail.com>
2017-11-16 12:26:54 +0000

    configure enviroment

---
+++ src/js/test.js
@@ -0,0 +1,11 @@
+function Example () {
+    const test = () => {
+        return "test";
+    };
+
+    return {
+        test: test
+    }
+}
+
+module.exports = Example;
---
+++ test/test.spec.js
@@ -0,0 +1,9 @@
+const assert = require("chai").assert;
+
+describe("test", () => {
+   
+    it("true is true", () => {
+       assert.isTrue(true) 
+    });
+    
+});
